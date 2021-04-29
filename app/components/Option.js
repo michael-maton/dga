6c9b44f865b717e01b01ef151a4302adff3338b9
michael-maton <mjmaton6@gmail.com>
2021-04-29 04:01:07 -0500

    options mapped over, diaplying in spinner windows

---
+++ app/components/Option.js
@@ -0,0 +1,76 @@
+import React, { Component } from "react";
+import { StyleSheet, View, Text } from "react-native";
+import Constants from "../../Constants";
+
+export default class Option extends Component {
+  constructor(props) {
+    super(props);
+    this.option = this.props.optionObject.option;
+    this.id = this.props.optionObject.id;
+    this.state = {
+      color: null,
+      backgroundColor: null,
+    };
+  }
+
+  getColorScheme = () => {
+    let colorID = this.id;
+    if (colorID === 1) {
+      this.state.color = "beige";
+      this.state.backgroundColor = "gray";
+    } else if (colorID === 2) {
+      this.state.color = "yellow";
+      this.state.backgroundColor = "red";
+    } else if (colorID === 3) {
+      this.state.color = "white";
+      this.state.backgroundColor = "purple";
+    } else if (colorID === 4) {
+      this.state.color = "orange";
+      this.state.backgroundColor = "violet";
+    } else if (colorID === 5) {
+      this.state.color = "crimson";
+      this.state.backgroundColor = "dodgerblue";
+    } else if (colorID === 6) {
+      this.state.color = "gold";
+      this.state.backgroundColor = "purple";
+    } else if (colorID === 7) {
+      this.state.color = "purple";
+      this.state.backgroundColor = "yellow";
+    }
+  };
+
+  optionStyle = () => {
+    return {
+      backgroundColor: this.state.backgroundColor,
+      width: Constants.MAX_WIDTH / 2,
+      height: Constants.MAX_HEIGHT / 7,
+      justifyContent: "center",
+      alignItems: "center",
+    };
+  };
+
+  render() {
+    this.getColorScheme();
+    return (
+      <View style={this.optionStyle()}>
+        <Text
+          style={{
+            color: this.state.color,
+            fontSize: 20
+          }}
+        >
+          {this.option}
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  optionContainer: {
+    width: Constants.MAX_WIDTH / 2,
+    height: Constants.MAX_HEIGHT / 7,
+    alignItems: "center",
+    borderRadius: 100,
+  },
+});
