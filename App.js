a335f0862714794456542ab0ee6d91bb0c575b78
michael-maton <mjmaton6@gmail.com>
2021-04-29 00:36:38 -0500

    main spin game screen initialized, placeholder styling, constants added

--- App.js
+++ App.js
@@ -1,21 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+} from "react-native";
+import Spinner from "./app/components/Spinner";
+import Constants from "./Constants";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.appContainer}>
+      <View style={styles.headerContainer}>
+        <Text style={styles.headerText}>Select your shot</Text>
+      </View>
+      <View style={styles.spinnerContainer}>
+        <Spinner />
+        <Spinner />
+      </View>
+      <TouchableOpacity style={styles.spinButton} onPress={() => {}}>
+        <Text style={styles.button}> Spin </Text>
+      </TouchableOpacity>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContainer: {
+    height: Constants.MAX_HEIGHT,
+    width: Constants.MAX_WIDTH,
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    backgroundColor: "#565388",
+  },
+  headerContainer: {
+    height: "10%",
+    width: Constants.MAX_WIDTH,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  headerText: {
+    fontSize: 40,
+    // fontWeight: "bold",
+    color: "#C4BE6B",
+  },
+  spinnerContainer: {
+    width: Constants.MAX_WIDTH,
+    height: Constants.MAX_HEIGHT / 2,
+    backgroundColor: "#565388",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    borderTopWidth: 10,
+    borderTopColor: "#C4BE6B",
+    borderBottomWidth: 10,
+    borderBottomColor: "#C4BE6B",
+  },
+  spinButton: {
+    width: Constants.MAX_WIDTH / 2.5,
+    height: Constants.MAX_HEIGHT / 12.5,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#7F7BA7",
+    borderRadius: 10,
+  },
+  button: {
+    color: "#C4BE6B",
+    fontSize: 20,
+    fontWeight: "bold",
   },
 });
