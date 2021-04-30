e04cdec05c02a6da7ad4fb0881cc07cba49bbced
michael-maton <mjmaton6@gmail.com>
2021-04-29 20:23:13 -0500

    spinning funcitonality working for one spinner

--- app/components/Spinner.js
+++ app/components/Spinner.js
@@ -6,25 +6,50 @@ import OptionList from "./OptionList";
 export default class Spinner extends Component {
   constructor(props) {
     super(props);
-    this.data = this.props.spinnerData;
+    // this.data = this.props.spinnerData;
+    this.state = {
+      width: Constants.MAX_WIDTH / 1.75,
+      height: Constants.MAX_HEIGHT / 7,
+    };
+    this.optionSet = null;
   }
 
+  spin = () => {
+    //   console.log(this.optionSet)
+    this.optionSet.scrollByOffset(6);
+  };
+
   render() {
+    // finds length of optionsList
+    // Array.apply(null, Array(this.props.spinnerData.length)).map((el, idx) => {
+    //   this.optionsList[idx] = idx;
+    // });
+    // console.log(this.optionsList);
     return (
-        <View style={styles.reel}>
-            <OptionList options={this.data}/>
-        </View>
+      <View
+        style={[
+          styles.reel,
+          { width: this.state.width, height: this.state.height },
+        ]}
+      >
+        <OptionList
+          options={this.props.spinnerData}
+          height={this.state.height}
+          width={this.state.width}
+          ref={(ref) => {
+            this.optionSet = ref;
+          }}
+        />
+      </View>
     );
   }
 }
+
 const styles = StyleSheet.create({
   reel: {
-    width: Constants.MAX_WIDTH / 1.75,
-    height: Constants.MAX_HEIGHT / 7,
     backgroundColor: "#C4BE6B",
     alignItems: "center",
     borderRadius: 10,
-
     overflow: "hidden",
   },
 });
