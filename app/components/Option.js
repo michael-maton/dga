571f7048bb0982894f02ec0752f46f24b333d185
michael-maton <mjmaton6@gmail.com>
2021-05-11 22:48:06 -0500

    cleaned up commented out code, styled with prettier

--- app/components/Option.js
+++ app/components/Option.js
@@ -5,54 +5,14 @@ import Constants from "../../Constants";
 export default class Option extends Component {
   constructor(props) {
     super(props);
-    // this.option = this.props.optionObject.option;
-    // this.id = this.props.optionObject.id;
     this.state = {
       color: "#5B7A58",
       backgroundColor: "#D3C7D7",
     };
   }
 
-  getColorScheme = () => {
-    // let colorID = this.props.optionObject.shot_id;
-    // if (colorID % 2 === 1) {
-    //   this.state.color = "beige";
-    //   this.state.backgroundColor = "gray";
-    // } else if (colorID % 2 === 0) {
-    //   this.state.color = "yellow";
-    //   this.state.backgroundColor = "red";
-    // }
-
-    // if (colorID === 1) {
-    //   this.state.color = "beige";
-    //   this.state.backgroundColor = "gray";
-    // } else if (colorID === 2) {
-    //   this.state.color = "yellow";
-    //   this.state.backgroundColor = "red";
-    // } else if (colorID === 3) {
-    //   this.state.color = "white";
-    //   this.state.backgroundColor = "purple";
-    // } else if (colorID === 4) {
-    //   this.state.color = "orange";
-    //   this.state.backgroundColor = "violet";
-    // } else if (colorID === 5) {
-    //   this.state.color = "crimson";
-    //   this.state.backgroundColor = "dodgerblue";
-    // } else if (colorID === 6) {
-    //   this.state.color = "gold";
-    //   this.state.backgroundColor = "purple";
-    // } else if (colorID === 7) {
-    //   this.state.color = "purple";
-    //   this.state.backgroundColor = "yellow";
-    // } else if (colorID === 8) {
-    //   this.state.color = "magenta";
-    //   this.state.backgroundColor = "dodgerblue";
-    // }
-  };
-
   optionStyle = () => {
     return {
-    //   backgroundColor: this.state.backgroundColor,
       width: Constants.MAX_WIDTH / 2,
       height: Constants.MAX_HEIGHT / 7,
       justifyContent: "center",
@@ -61,7 +21,6 @@ export default class Option extends Component {
   };
 
   render() {
-    this.getColorScheme();
     return (
       <View style={this.optionStyle()}>
         <Text
--- app/components/Spinner.js
+++ app/components/Spinner.js
@@ -14,20 +14,13 @@ export default class Spinner extends Component {
   }
 
   randomChoice = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1) + min)
-  }
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
 
   spin = () => {
-    this.optionSet.scrollByOffset(this.randomChoice(7, (Constants.PATTERN.length*Constants.REPEAT)/3));
-    // this.optionSet.scrollByOffset(this.randomChoice(
-    //     (Constants.REPEAT - 6) * Constants.PATTERN.length,
-    //     (Constants.REPEAT - 5) * Constants.PATTERN.length
-    // ));
-    // let multiplier = this.randomChoice(0, 5);
-    // console.log(this.randomChoice(
-    //         (Constants.REPEAT - 6) * Constants.PATTERN.length,
-    //         (Constants.REPEAT - 2) * Constants.PATTERN.length
-    //     ))
+    this.optionSet.scrollByOffset(
+      this.randomChoice(7, (Constants.PATTERN.length * Constants.REPEAT) / 3)
+    );
   };
 
   render() {
@@ -56,7 +49,6 @@ const styles = StyleSheet.create({
     backgroundColor: "#C4BE6B",
     alignItems: "center",
     justifyContent: "center",
-    // height: Constants.MAX_HEIGHT / 3,
     borderRadius: 15,
     overflow: "hidden",
     borderWidth: 7,
