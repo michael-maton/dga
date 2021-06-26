0b8d0ea73d016922139e42eb820602da42ac8eba
michael-maton <mjmaton6@gmail.com>
2021-06-26 04:11:42 -0500

    landing page temp styling, skip button added to skip signin/create account and go straight to game

--- app/Screens/Landing/LandingPage.js
+++ app/Screens/Landing/LandingPage.js
@@ -1,27 +1,81 @@
 import React from "react";
-import { SafeAreaView, Text, Button, StyleSheet } from "react-native";
+import {
+  SafeAreaView,
+  View,
+  Text,
+  Button,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+
 import Constants from "../../../Constants";
 import CreateAccount from "./CreateAccount";
 import SignIn from "./SignIn";
 
 const styles = StyleSheet.create({
   appContainer: {
-    height: Constants.MAX_HEIGHT,
+    height: Constants.MAX_HEIGHT - 70,
     width: Constants.MAX_WIDTH,
     alignItems: "center",
     justifyContent: "center",
     backgroundColor: "#76b6ef",
+  },
+  buttonContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 50,
+  },
+  authButtons: {
+    alignItems: "center",
+    justifyContent: "center",
+    width: 150,
+    elevation: 8,
+    backgroundColor: "#F2A007",
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 12,
+    margin: 5,
+  },
+  appButtonText: {
+    fontSize: 18,
+    color: "#fff",
+    fontWeight: "bold",
+    alignSelf: "center",
+    textTransform: "uppercase",
   },
 });
 
 export default function Landing({ navigation }) {
   return (
     <SafeAreaView style={styles.appContainer}>
-      {/* <Text>Sign In Screen</Text> */}
-      <Button title="Sign In" onPress={() => alert("todo")} />
+      <LinearGradient
+        colors={["#76b6ef", "#C9CFF2", "#F2B8A2", "#F2A007"]}
+        style={{
+          position: "absolute",
+          height: Constants.MAX_HEIGHT,
+          width: Constants.MAX_WIDTH,
+        }}
+      />
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          style={styles.authButtons}
+          title="Sign In"
+          onPress={() => alert("todo")}
+        >
+          <Text style={styles.authButtonText}>Sign In</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.authButtons}
+          title="Create Account"
+          onPress={() => navigation.push("CreateAccount")}
+        >
+          <Text style={styles.authButtonText}>Create Account</Text>
+        </TouchableOpacity>
+      </View>
       <Button
-        title="Create Account"
-        onPress={() => navigation.push("CreateAccount")}
+        title="Skip"
+        onPress={() => navigation.push("SlotsSelectorGame")}
       />
     </SafeAreaView>
   );
