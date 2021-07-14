197da3d396feab5735a5396badf471a1fe751f70
Michael Maton <mjmaton6@gmail.com>
2021-07-14 18:49:43 -0500

    settings page setup complete

--- app/Screens/ShotSelectorGame/Settings.js
+++ app/Screens/ShotSelectorGame/Settings.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { SafeAreaView, Text, View, StyleSheet } from "react-native";
-import Constants from "../../../../Constants";
+import Constants from "../../../Constants";
 
 export default class Settings extends Component {
   constructor(props) {
@@ -9,7 +9,7 @@ export default class Settings extends Component {
 
   render() { 
     return (
-      <SafeAreaView >
+      <SafeAreaView style={styles.settingsWrapper}>
           <Text>Settings Page</Text>
       </SafeAreaView>
     );
@@ -17,4 +17,10 @@ export default class Settings extends Component {
 }
 
 const styles = StyleSheet.create({
+    settingsWrapper: {
+        height: Constants.MAX_HEIGHT,
+        width: Constants.MAX_WIDTH,
+        alignItems: "center",
+        backgroundColor: "#76b6ef",
+    },
 });
