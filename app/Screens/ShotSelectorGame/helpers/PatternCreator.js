745f17a6bcad38fa5465baa8c4a86826856e8ff0
Michael Maton <mjmaton6@gmail.com>
2021-07-19 17:46:58 -0500

    fixed patternCreator function from selecting wrong shots

--- app/Screens/ShotSelectorGame/helpers/PatternCreator.js
+++ app/Screens/ShotSelectorGame/helpers/PatternCreator.js
@@ -3,14 +3,16 @@ export default function PatternCreator(data) {
   let pattern = "";
 
   data.forEach((e) => {
-    patternArr.push(e.luck);
+    patternArr.push([e.shot_id, e.luck]);
   });
+  // patternArr = patternArr.filter((item) => item[1] !== 0);
+  // console.log(patternArr)
 
   while (patternArr.length > 0) {
-    patternArr = patternArr.filter((item) => item !== 0);
+    patternArr = patternArr.filter((item) => item[1] !== 0);
     for (let i = 0; i <= patternArr.length - 1; i++) {
-      pattern += data[i].shot_id.toString();
-      patternArr[i] -= 1;
+      pattern += patternArr[i][0].toString();
+      patternArr[i][1] -= 1;
     }
   }
 
@@ -18,5 +20,6 @@ export default function PatternCreator(data) {
     pattern = pattern.repeat(2);
   }
   
+  // console.log(pattern)
   return pattern;
 }
