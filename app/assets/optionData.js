e85bee8baf21878a459aa9bb225f77d068416939
Michael Maton <mjmaton6@gmail.com>
2021-07-23 15:04:24 -0500

    added new key in data for disabled options -- takes place of 0 luck

--- app/assets/optionData.js
+++ app/assets/optionData.js
@@ -4,46 +4,55 @@ const optionData = {
       shot_id: 1,
       option: "Overstable",
       luck: 5,
+      disabled: false,
     },
     {
       shot_id: 2,
       option: "Understable",
       luck: 5,
+      disabled: false,
     },
     {
       shot_id: 3,
       option: "Putter",
       luck: 5,
+      disabled: false,
     },
     {
       shot_id: 4,
       option: "Mid-range",
       luck: 5,
+      disabled: false,
     },
     {
       shot_id: 5,
       option: "Fairway Driver",
       luck: 5,
+      disabled: false,
     },
     {
       shot_id: 6,
       option: "Distance Driver",
       luck: 5,
+      disabled: false,
     },
     {
       shot_id: 7,
       option: "Opponents Choice",
       luck: 2,
+      disabled: false,
     },
     {
       shot_id: 8,
       option: "Wild",
       luck: 1,
+      disabled: false,
     },
     {
       shot_id: 9,
       option: "No data",
       luck: 0,
+      disabled: false,
     },
   ],
   shotType: [
@@ -51,46 +60,55 @@ const optionData = {
       shot_id: 1,
       option: "Forehand",
       luck: 5,
+      disabled: false,
     },
     {
       shot_id: 2,
       option: "Backhand",
       luck: 5,
+      disabled: false,
     },
     {
       shot_id: 3,
       option: "Roller",
       luck: 5,
+      disabled: false,
     },
     {
       shot_id: 4,
       option: "Hyzer",
       luck: 5,
+      disabled: false,
     },
     {
       shot_id: 5,
       option: "Anhyzer",
       luck: 5,
+      disabled: false,
     },
     {
       shot_id: 6,
       option: "Overhand",
       luck: 5,
+      disabled: false,
     },
     {
       shot_id: 7,
       option: "Opponents Choice",
       luck: 2,
+      disabled: false,
     },
     {
       shot_id: 8,
       option: "Opposite Hand",
       luck: 1,
+      disabled: false,
     },
     {
       shot_id: 9,
       option: "No data",
       luck: 0,
+      disabled: false,
     },
   ],
 };
@@ -142,7 +160,6 @@ export default optionData;
     Backhand, Forehand,  
 
 */
-
 
 /* 
     PATTERN CREATION
